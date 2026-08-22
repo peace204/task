f8764510bc83b93c7e1e00a4c1b6d654cbdcc5da
Peace Onyenweze <onyenwezechinwendupeace@gmail.com>
2026-08-22 08:27:51 +0100

    Set up SpatialHunt project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Showcase from '@/component/home/showcase'
+
+const page = () => {
+  return (
+    <div>
+        <Showcase />
+    </div>
+  )
+}
+
+export default page
